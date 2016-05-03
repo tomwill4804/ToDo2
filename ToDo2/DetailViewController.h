700e59aa0defc1c29e78966d08fbc5ed229e398c
Tom Williamson <tom@dtssoftware.com>
2016-05-03 13:24:23 -0400

    Initial Commit

---
+++ ToDo2/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  ToDo2
+//
+//  Created by Tom Williamson on 5/3/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ ToDo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDo2
+//
+//  Created by Tom Williamson on 5/3/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
